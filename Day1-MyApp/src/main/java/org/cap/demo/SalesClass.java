69968a91de6346213a52b4b16e7ce4db27a8cc18
caprepo <vidavid@in-pnq-coe11.corp.capgemini.com>
2017-11-23 16:20:22 +0530

    My First Commit.

---
+++ Day1-MyApp/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class SalesClass {
+
+	int[] arr={1,2,3,4,5,6};
+	
+	public void display(){
+		System.out.println("Array Elements");
+		for(int n: arr){
+			System.out.println(n);
+		}
+	}
+	
+}
